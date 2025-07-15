31cc6329829f0a194483d17d1e4f531e62e0c04a
Anu Kherlen <anukherlen@Anus-MacBook-Pro-3.local>
2025-07-15 14:15:15 -0500

    initial file upload

---
+++ src/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+
+const Home = () => {
+  return (
+    <div>
+        <img src="/home.jpg" alt="Anuka Photos Banner" className="banner-img" />
+
+    </div>
+  )
+}
+
+export default Home
